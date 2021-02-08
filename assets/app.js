02f601a253b30d18206846402cea845b4e58795f
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2021-02-08 11:13:03 +0100

    Add dependencies

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,21 @@
 import './styles/app.scss';
+import 'bootstrap';
 
-import $ from 'cash-dom';
+import 'datatables.net';
+import 'datatables.net-dt/js/dataTables.dataTables';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+import $ from 'jquery';
+import Routing from '../public/bundles/fosjsrouting/js/router.min.js';
+
+global.$ = $;
+global.Routing = Routing;
+
+const routes = require('../public/generated/routes.json');
+Routing.setRoutingData(routes);
+
+$(document).ready(function() {
+    $('#example').dataTable( {
+        "order": [[ 3, "desc" ]]
+    } );
+} );
