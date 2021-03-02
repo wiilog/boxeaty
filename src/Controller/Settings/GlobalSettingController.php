7c9f4ab82813af5be831a16201b19834aeb35793
t.gentile <thomas.gentile@wiilog.fr>
2021-03-02 12:39:00 +0100

    Fix global settings issue

--- src/Controller/Settings/GlobalSettingController.php
+++ src/Controller/Settings/GlobalSettingController.php
@@ -24,8 +24,8 @@ class GlobalSettingController extends AbstractController {
             "csv_encoding" => $settings[GlobalSetting::CSV_EXPORTS_ENCODING],
             "setting_code" => $settings[GlobalSetting::SETTING_CODE],
             "empty_kiosk_code" => $settings[GlobalSetting::EMPTY_KIOSK_CODE],
-            "box_capacities" => explode(",", $settings[GlobalSetting::BOX_CAPACITIES]) ?: [],
-            "box_shapes" => explode(",", $settings[GlobalSetting::BOX_SHAPES]) ?: [],
+            "box_capacities" => explode(",", $settings[GlobalSetting::BOX_CAPACITIES]->getValue()) ?: [],
+            "box_shapes" => explode(",", $settings[GlobalSetting::BOX_SHAPES]->getValue()) ?: [],
         ]);
     }
 
