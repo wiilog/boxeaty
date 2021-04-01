5aa5a664dcc40e7737e83f968a77ca25ee33a3d8
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2021-04-01 17:06:51 +0200

    Fix boxeaty register

--- assets/pages/security.js
+++ assets/pages/security.js
@@ -1,6 +1,13 @@
 import $ from "jquery";
+import "arrive";
+import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 
-import '../select2';
+import "../jquery";
+import "../select2";
+
+const routes = require(`../../public/generated/routes.json`);
+Routing.setRoutingData(routes);
+global.Routing = Routing;
 
 //password toggle eye icon
 $(document).on(`click`, `.show-password span`, function() {
