eefd16837cb9997733199250edf51156663deda6
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-08-27 18:25:08 +0200

    fix error 500

--- migrations/Version20210730132215.php
+++ migrations/Version20210730132215.php
@@ -19,19 +19,21 @@ final class Version20210730132215 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE delivery_method ADD type INTEGER NOT NULL');
-        $deliveryMethods = $this->connection->executeQuery("SELECT * FROM delivery_method;");
-        foreach ($deliveryMethods as $deliveryMethod) {
-            switch ($deliveryMethod['icon']) {
-                case "bike":
-                    $this->addSql("UPDATE delivery_method SET type = 0 WHERE id = ${deliveryMethod['id']}");
-                    break;
-                case "light-truck":
-                    $this->addSql("UPDATE delivery_method SET type = 1 WHERE id = ${deliveryMethod['id']}");
-                    break;
-                case "heavy-truck":
-                    $this->addSql("UPDATE delivery_method SET type = 2 WHERE id = ${deliveryMethod['id']}");
-                    break;
+        if ($schema->hasTable("delivery_method")) {
+            $this->addSql('ALTER TABLE delivery_method ADD type INTEGER NOT NULL');
+            $deliveryMethods = $this->connection->executeQuery("SELECT * FROM delivery_method;");
+            foreach ($deliveryMethods as $deliveryMethod) {
+                switch ($deliveryMethod['icon']) {
+                    case "bike":
+                        $this->addSql("UPDATE delivery_method SET type = 0 WHERE id = ${deliveryMethod['id']}");
+                        break;
+                    case "light-truck":
+                        $this->addSql("UPDATE delivery_method SET type = 1 WHERE id = ${deliveryMethod['id']}");
+                        break;
+                    case "heavy-truck":
+                        $this->addSql("UPDATE delivery_method SET type = 2 WHERE id = ${deliveryMethod['id']}");
+                        break;
+                }
             }
         }
     }
